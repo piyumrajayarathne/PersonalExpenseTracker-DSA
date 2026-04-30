a7c0a81b72583cdaa1b5dce1483eb774c6fa5a8a
piyumrajayarathne <piyumrajayarathne@gmail.com>
2026-04-30 18:45:59 +0530

    Add category management with total update functionality

--- include/HashTable.h
+++ include/HashTable.h
@@ -1,6 +1,9 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
+#include <string>
+using namespace std;
 
 void showCategorySummary();
+void updateCategory(string category, float amount, string type);
 
 #endif
--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -7,3 +7,25 @@ void showCategorySummary() {
     cout << "Category summary " << endl;
 }
 
+string categories[10];
+float totals[10];
+int sizeT = 0;
+
+int findCategory(string c) {
+    for(int i = 0; i < sizeT; i++) {
+        if(categories[i] == c) return i;
+    }
+    return -1;
+}
+
+void updateCategory(string category, float amount, string type) {
+    int index = findCategory(category);
+
+    if(index == -1) {
+        categories[sizeT] = category;
+        totals[sizeT] = (type == "expense") ? amount : -amount;
+        sizeT++;
+    } else {
+        totals[index] += (type == "expense") ? amount : -amount;
+    }
+}
